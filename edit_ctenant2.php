ff27c16c7e46b13f910f9906cd90efb4c727cbcb
Lakmini Gunarathna <lakmini17gunarathna@gmail.com>
2022-07-17 19:20:32 +0530

    Update edit_ctenant2.php
    check this

--- edit_ctenant2.php
+++ edit_ctenant2.php
@@ -5,7 +5,7 @@ if(isset($_POST['update']))
     $id = $_POST['id'];
     $projname = $_POST['projnm'];
 
-    $query = "UPDATE `ctenants` SET `projnm` = ".$projnm." WHERE `ctenants`.`id` = $id";
+    $query = "UPDATE `ctenants` SET `projnm` = ".$projname." WHERE `ctenants`.`id` = $id";
 
     $result = mysqli_query($conn, $query)
 
@@ -17,19 +17,27 @@ if(isset($_POST['update']))
 
     mysqli_close($conn);
 }
+?>
 
+<?php 
+    $sql = "SELECT *  FROM ctenants where id = {$_GET['id']} ";
+    $result = $conn->query($sql);
 
+    if ($result->num_rows > 0) {
+    
+    while($row = $result->fetch_assoc()) {
 
 ?>
-<form action="edit_ctenant2.php" method="post">
+
+<form action="" method="post">
     <div class="row">
         <div class="col-md-6">
             
             <div class="form-group">
                 <label for="id">ID</label>
-                <input type="text" class="form-control" id="id" name="id" value="">
+                <input type="text" class="form-control" id="id" name="id" value="<?php echo $row['id'] ?>">
                 <label for="projnm">Project Name</label>
-                <input type="text" class="form-control" id="projnm" name="projnm" value="">
+                <input type="text" class="form-control" id="projnm" name="projnm" value="<?php echo $row['projnm'] ?>">
             </div>
         </div>
 
@@ -38,3 +46,11 @@ if(isset($_POST['update']))
 </form>
 
 
+<?php
+    }
+    } else {
+    echo "0 results";
+    }
+
+
+?>
