e40f282d7743cbb2958da84a387e66f13edc669d
DeadlyXC <isurucso94@gmail.com>
2022-07-25 14:46:03 +0530

    ui fix ong2

--- Ongoing Projects2.php
+++ Ongoing Projects2.php
@@ -3,8 +3,8 @@
 <head>
     <title></title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script> -->
     <style>
         body {
             margin: 0;
@@ -13,7 +13,7 @@
         }
 
         .box {
-            /* width: 1270px; */
+            width: 1270px;
             padding: 20px;
             background-color: #fff;
             border: 1px solid #ccc;
